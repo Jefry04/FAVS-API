320b344d3c384c770f754e5efe1181d0c6da0857
Jefferson <jefry04@gmail.com>
2022-06-15 20:49:24 -0500

    Create conection with db

---
+++ api/db.js
@@ -0,0 +1,22 @@
+const mongoose = require ("mongoose");
+
+function connect (){
+  const host = process.env.DB_HOST;
+  const port = process.env.DB_PORT;
+  const db = process.env.DB_DATABASE;
+  const uri = `${host}:${port}/${db}`;
+
+  mongoose.connect(uri);
+
+  mongoose.connection.once('open', () => {
+    console.log(`Connection with mongo in ${host}:${port} with DB:${db}`);
+  });
+
+  mongoose.connection.on('error', (err) => {
+    console.log('Something went wrong!', err);
+  });
+
+  return mongoose.connection;
+}
+
+module.exports = { connect };
--- api/index.js
+++ api/index.js
@@ -1,14 +1,18 @@
 require('dotenv').config();
 const express = require("express");
 const cors = require ("cors");
+const { connect } = require("./db")
 
 const app = express();
 
 const port = process.env.APP_PORT;
 const host = process.env.APP_URL;
 
+connect();
+
 app.use(express.json());
+app.use(cors());
 
 app.listen (port, ()=>{
-    console.log(`App running in ${host}:${port}`)
+  console.log(`App running in ${host}:${port}`)
 })
