f0c871e9527b398ef4afacdc3a4184b4695b2196
Jefferson <jefry04@gmail.com>
2022-06-21 15:20:51 -0500

    Finish CRUD

--- api/controllers/list.controller.js
+++ api/controllers/list.controller.js
@@ -3,10 +3,9 @@ const User = require("../models/user.model");
 
 module.exports = {
   async list(req, res) {
-
     try {
       const userId = req.user;
-      const lists = await List.find({ 'userId': userId} );
+      const lists = await List.find({ userId: userId });
 
       res.status(200).json({ message: "List found", lists });
     } catch (error) {
@@ -37,6 +36,75 @@ module.exports = {
       }
     } catch (err) {
       res.status(400).json(err);
+    }
+  },
+
+  async getSingleList(req, res) {
+    try {
+      const userId = req.user;
+      const { listId } = req.params;
+
+      const list = await List.findById(listId);
+
+      if (list.userId.toString() !== userId) {
+        res.status(403).json({ message: "unauthorized user" });
+        return;
+      }
+
+      res.status(200).json({ message: "List found", list });
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  },
+
+  async update(req, res) {
+    try {
+      const { listId } = req.params;
+      const userId = req.user;
+      const { fav } = req.body;
+
+      const list = await List.findById(listId);
+
+      if (!list) {
+        res.status(404).json({ message: "List not found." });
+        return;
+      }
+      if (list.userId.toString() !== userId) {
+        res.status(403).json({ message: "unauthorized user" });
+        return;
+      }
+
+      list.fav = [...list.fav, ...fav]
+      await list.save({ validateBeforeSave: false });
+
+
+      res.status(200).json({ message: "List updated", list });
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  },
+
+  async destroy(req, res) {
+    try {
+      const userId = req.user;
+      const { listId } = req.params;
+
+      const list = await List.findById(listId);
+
+      if (!list) {
+        res.status(404).json({ message: "List not found." });
+        return;
+      }
+      if (list.userId.toString() !== userId) {
+        res.status(403).json({ message: "unauthorized user" });
+        return;
+      }
+
+      await List.deleteOne({ _id: listId });
+
+      res.status(200).json({ message: "List deleted", list });
+    } catch (error) {
+      res.status(400).json(error);
     }
   },
 };
--- api/routes/list.js
+++ api/routes/list.js
@@ -5,5 +5,8 @@ const { userAuth } = require ("../middlewares/userAuth.middleware");
 router.route("/").get(userAuth, listController.list);
 router.route("/").post(userAuth, listController.create);
 
+router.route("/:listId").get(userAuth, listController.getSingleList);
+router.route("/:listId").delete(userAuth, listController.destroy);
+router.route("/:listId").put(userAuth, listController.update);
 
 module.exports = router;
