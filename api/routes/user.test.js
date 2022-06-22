2ce790b1409e886d24a0d4d46541fffdc10939e3
Jefferson <jefry04@gmail.com>
2022-06-21 22:47:32 -0500

    Create test for list controller

--- api/routes/user.test.js
+++ api/routes/user.test.js
@@ -1,30 +1,47 @@
+const clonServer = require("supertest");
 const req = require("supertest");
-const { connect, disconected, cleanup } = require ("../db");
-const app = require ("../app");
-const { disconnect } = require("mongoose");
+const { connect, disconected, cleanup } = require("../db");
+const User = require("../models/user.model");
+const app = require("../app");
 
-describe ("App", () => {
+describe("App", () => {
   beforeAll(async () => {
     await connect();
-  })
+  });
 
   beforeEach(async () => {
     await cleanup();
-  })
+  });
 
-  afterAll (async () =>{
+  afterAll(async () => {
     await disconected();
-  })
+  });
 
-  it ("should create a user", async () =>{
+  it("should create a user", async () => {
     const user = {
-        email:"correo@correo.com",
-        password:"Clave123*",
-        confirmPassword: "Clave123*"
-    }
-    const res = await req(app).post("/auth/local/signup").send(user);
+      email: "correo@correo.com",
+      password: "Clave123*",
+      confirmPassword: "Clave123*",
+    };
+    const res = await clonServer(app).post("/auth/local/signup").send(user);
 
     expect(res.statusCode).toBe(200);
-    expect(res.body).toHaveProperty("token")
+    expect(res.body).toHaveProperty("token");
   });
+
+  it("should not create user when there is no email", async () => {
+    const user = { password: "Clave123*", confirmPassword: "Clave123*" };
+    const res = await clonServer(app).post("/auth/local/signup").send(user);
+
+    expect(res.statusCode).toBe(400);
+  });
+
+  // it ("should login successfully ", async () =>{
+  //   const user = { email: "test@test.com", password: "Clave123*" };
+  //   await User.create(user);
+  //   const res = await clonServer(app).post("/auth/local/login").send(user);
+
+  //   expect(res.statusCode).toBe(200);
+  //   expect(res.body).toHaveProperty("token");
+  // });
 });
